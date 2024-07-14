37601368557b365115bd076864dc480c4cc0f5a2
Recep <49814995+Recepurkun@users.noreply.github.com>
2024-07-14 19:35:02 +0300

    config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB7m4oGpTSMPtPs8vwzbNoN_rmd3UG6ycs",
-    authDomain: "e-commerce-shopco.firebaseapp.com",
-    projectId: "e-commerce-shopco",
-    storageBucket: "e-commerce-shopco.appspot.com",
-    messagingSenderId: "1034766579335",
-    appId: "1:1034766579335:web:a62d379e8b156f469f6a22",
-    measurementId: "G-DSZ7EEPB4C",
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
